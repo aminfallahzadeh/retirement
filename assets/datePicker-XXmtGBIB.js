4b3fe4a552889bfc3544e18ab2bcf25552fe906e
aminfallahzadeh <aminfallahzadeh@users.noreply.github.com>
2024-12-18 12:33:45 +0000

    deploy: 8fd72959b77c042c4f49e4ddb455d40730e7eb5e

---
+++ assets/datePicker-XXmtGBIB.js
@@ -0,0 +1,1 @@
+const r={border:"none",cursor:"pointer"},e={border:"1px solid var(--color-input-border)",width:"100%",height:"100%",paddingLeft:"20px",borderRadius:"4px",cursor:"pointer"};export{e as a,r as d};
