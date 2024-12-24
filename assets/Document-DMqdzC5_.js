ba884139a07ad01bfaed29e9d3d264be78b55820
aminfallahzadeh <aminfallahzadeh@users.noreply.github.com>
2024-12-24 14:06:21 +0000

    deploy: bd7f4240c443fea183862e23059f03ff0d354a65

---
+++ assets/Document-DMqdzC5_.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D94qalpB.js";const o=()=>t.jsx("div",{children:"Document"});export{o as default};
---
+++ assets/DoneOutlined-maKsW4ks.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./index-D94qalpB.js";const n=o(t.jsx("path",{d:"M9 16.2 4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4z"}),"DoneOutlined");export{n as D};
