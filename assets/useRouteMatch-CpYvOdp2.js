fa74d8d0648d5bf8398fb12efdf66a7a55672bbb
aminfallahzadeh <aminfallahzadeh@users.noreply.github.com>
2024-12-26 18:21:26 +0000

    deploy: d667ca300539a91481ba1239ec4b35aa4ba12050

--- assets/useRouteMatch-CpYvOdp2.js
+++
@@ -1,1 +0,0 @@
-import{bN as s,ex as c}from"./index-CAx8cgCu.js";const u=o=>{const{pathname:n}=s();for(let t=0;t<o.length;t+=1){const a=o[t],e=c(a,n);if(e!==null)return e}return null};export{u};
---
+++ assets/useRouteMatch-DVwW4zOS.js
@@ -0,0 +1,1 @@
+import{cn as s,eG as c}from"./index-BXRanfZW.js";const u=n=>{const{pathname:e}=s();for(let t=0;t<n.length;t+=1){const a=n[t],o=c(a,e);if(o!==null)return o}return null};export{u};
